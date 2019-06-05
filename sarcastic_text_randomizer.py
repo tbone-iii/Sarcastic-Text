a57c360f9247e3a99ba84135db15a1ccb23106b7
thomas_arambula <thomasarambula96@gmail.com>
2019-06-04 17:54:04 -0700

    Initial script for sarcastic text randomizer. Includes "string_randomize" function.

---
+++ sarcastic_text_randomizer.py
@@ -0,0 +1,39 @@
+""" Sarcastic Text Randomizer
+Usage:
+  sarcastic_text_randomizer.py <string>
+  sarcastic_text_randomizer.py (-h | --help)
+  sarcastic_text_randomizer.py --version
+
+Options:
+  <string>      Any string to be randomly capitalized.
+  -h --help     Show this screen.
+  --version     Show version.
+
+"""
+
+from docopt import docopt
+from random import random
+
+# Feature(s) to add.
+# TODO:
+# ! 1) Automatically add the result to the clipboard.
+
+
+def string_randomize(string: str) -> str:
+    """ Takes a string and randomly capitalizes the characters. """
+
+    string = string.lower()
+    letter_list = []
+    for letter in string:
+        # Serves to give a random boolean output (i.e. 0 or 1)
+        if round(random()):
+            letter_list.append((letter.upper()))
+        else:
+            letter_list.append((letter))
+
+    return(''.join(letter_list))
+
+
+if __name__ == "__main__":
+    arguments = docopt(__doc__, version='Sarcastic Text Randomizer 1.0')
+    print(string_randomize(arguments["<string>"]))
