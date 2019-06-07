3f6c61520b54880ddaad55e04f8245cd58b6a7e7
thomas_arambula <thomasarambula96@gmail.com>
2019-06-06 23:11:54 -0700

    Properly takes in optional input commands. Adds process to hang the shell.

--- sarcastic_text_randomizer.py
+++ sarcastic_text_randomizer.py
@@ -1,11 +1,11 @@
 """ Sarcastic Text Randomizer
 Usage:
-  sarcastic_text_randomizer.py <string>
+  sarcastic_text_randomizer.py [<string>]
   sarcastic_text_randomizer.py (-h | --help)
   sarcastic_text_randomizer.py --version
 
 Options:
-  <string>      Any string to be randomly capitalized.
+  [<string>]    Any string to be randomly capitalized [default: ""].
   -h --help     Show this screen.
   --version     Show version.
 
@@ -36,4 +36,12 @@ def string_randomize(string: str) -> str:
 
 if __name__ == "__main__":
     arguments = docopt(__doc__, version='Sarcastic Text Randomizer 1.0')
-    print(string_randomize(arguments["<string>"]))
+    string = arguments["<string>"]
+    if string:
+        print(string_randomize(string))
+    else:
+        string = input('String to randomize: \n')
+        print(string_randomize(string))
+
+    # Hangs the subprocess to avoid closing immediately.
+    input()
